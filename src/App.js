be90e67914d543279959811fabc4fa8ae2f887e6
felipejoribeiro <feliperibeiro.ufu@gmail.com>
2021-06-08 20:33:11 -0300

    🏹 - teste git 4

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Now??
+          Learn right Now
         </a>
       </header>
     </div>
